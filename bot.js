fac1a4f7cd2de35bd3d8defa1a37a9c6a5859cd6
SenpaiSpooniel <59844507+SenpaiSpooniel@users.noreply.github.com>
2020-01-14 15:13:08 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.eDBB_sj_wMC1BrYOtTbId9Ujoroef2_Z);//BOT_TOKEN is the Client Secret
